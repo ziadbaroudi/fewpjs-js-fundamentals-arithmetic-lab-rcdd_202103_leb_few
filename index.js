22f26e9f60187fd9290264c4970b8789eedf101c
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-06 21:29:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1= 2;
+var num2= 31;
+var multiply = num1*num2;
+
+var random = Math.floor(Math.random() * 10) + 0;
+
+var num3= 4;
+var num4 = 7;
+var mod = num3%num4;
+
+var max = Math.max(1,2,3,20,6);
