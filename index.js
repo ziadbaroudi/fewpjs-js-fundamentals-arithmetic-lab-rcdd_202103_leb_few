ad9543a348bc1fe08eff94118a48b235c5335a57
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-06 21:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Write your code here
-num1= 2
-num2= 31
-multiply = num1*num2
-random = Math.random()
-num3= 4
-mod = num3%7
+num1= 2;
+num2= 31;
+multiply = num1*num2;
+random = Math.random();
+num3= 4;
+mod = num3%7;
